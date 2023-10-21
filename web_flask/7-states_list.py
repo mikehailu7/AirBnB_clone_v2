af3231698a22f06a550258f96dca3432951e8b4f
mikehailu7 <mikehailu7@gmail.com>
2023-10-21 03:29:27 -0700

    web flask

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -19,4 +19,4 @@ def state_list():
     return render_template("7-states_list.html", states=states)
 
 if __name__ == '__main__':
-        app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
