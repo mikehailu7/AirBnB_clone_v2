62a672ddca2ba6a8cc3a8783ed07d30ba007a52b
mikehailu7 <mikehailu7@gmail.com>
2023-10-21 02:49:31 -0700

    web flask

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,40 +1,32 @@
 #!/usr/bin/python3
 # Author: MikiasHailu
-""" This will add third view func that redirects and has default val for variable """
-
+""" This Sript that starts a Flask web application  """
 from flask import Flask
-
-
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/')
-def hello_world():
-    """ Returns some text. """
-    return 'Hello HBNB!'
+@app.route('/', strict_slashes=False)
+def hello_hbn():
+    """ This function will return Hello HBNB! """
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb')
-def hello():
-    """ Return other text. """
-    return 'HBNB'
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ This function will return HBNB """
+    return "HBNB"
 
 
-@app.route('/c/<text>')
-def c_text(text):
-    """ This will replace text with variable. """
-    text = text.replace('_', ' ')
-    return 'C {}'.format(text)
+@app.route('/c/<text>', strict_slashes=False)
+def text_var(text):
+    """ This function will display text variable passed in """
+    return "C {}".format(text.replace("_", " "))
 
 
-@app.route('/python/')
-@app.route('/python/<text>')
-def python_text(text='is cool'):
-    """ This will replace more text with another variable. """
-    text = text.replace('_', ' ')
-    return 'Python {}'.format(text)
-
-
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def text_var_python(text="is cool"):
+    """ This will function to display text variable, with default "is cool" """
+    return "Python {}".format(text.replace("_", " "))
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+        app.run(host='0.0.0.0', port=5000)
